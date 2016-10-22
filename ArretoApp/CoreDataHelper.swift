3c1dbd321b00733d23dc15060e922f3c4cd49087
Rudy Matos <rudymatos@Rudys-MacBook-Pro.local>
2016-10-22 12:59:18 -0400

    initial data

---
+++ ArretoApp/CoreDataHelper.swift
@@ -0,0 +1,20 @@
+//
+//  CoreDataHelper.swift
+//  ArretoApp
+//
+//  Created by Rudy E Matos on 10/22/16.
+//  Copyright © 2016 Bearded Gentleman. All rights reserved.
+//
+
+import Foundation
+
+
+class CoreDataHelper{
+    
+    
+    func addPlayer(name: String){
+        
+    }
+    
+}
+
