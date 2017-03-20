96ee8525d5622754ce320216b22e48bfd569bdea
Rudy Matos <rudymatos@Rudys-MacBook-Pro.local>
2017-03-19 20:09:19 -0400

    committing latest changes before v2.0

--- ArretoApp/DeleteBoardVC.swift
+++ ArretoApp/DeleteBoardVC.swift
@@ -10,32 +10,45 @@ import UIKit
 
 class DeleteBoardVC: UIViewController {
     
+    
+    @IBOutlet weak var cardView: UIView!
+    @IBOutlet weak var totalRecordsCount: UILabel!
+    @IBOutlet weak var creationDate: UILabel!
     private let gameImpl = GameImpl()
     private var currentBoard : Board?
+    var delegate : MainVCHandler?
+    private let viewHelper = ViewHelper.sharedInstance
     
     override func viewDidLoad() {
         super.viewDidLoad()
         configureApp()
-
-        
-        // Do any additional setup after loading the view.
+        configureView()
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func configureView(){
+        viewHelper.addShadow(toView: cardView)
+        if let count = currentBoard?.events?.count, let date = currentBoard?.createdOn{
+            totalRecordsCount.text = "\(count) registros encontrados"
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateStyle = .long
+            dateFormatter.timeStyle = .medium
+            dateFormatter.locale = Locale(identifier: "es-DO")
+            creationDate.text = "Esta pizarra fue creada \(dateFormatter.string(from: date  as Date))"
+        }
     }
     
-
+    func configureApp(){
+        currentBoard = gameImpl.getBoard()
+    }
+    
     @IBAction func deleteBoardDidTouch(_ sender: UIButton) {
         
-        
-        //First create alert and notify the user
         let deleteBoardAC = UIAlertController(title: "Borrar Pizarra", message: "Desea borrar la pizarra? Todas las entradas seran eliminadas", preferredStyle: .alert)
         let cancelAction = UIAlertAction(title: "Cancelar", style: .cancel, handler: nil)
         let deleteAction = UIAlertAction(title: "Si, estoy seguro!", style: .destructive, handler: { _ in
             self.gameImpl.clearBoard(board: self.currentBoard!)
             self.configureApp()
+            self.delegate?.removeAllRecordsHandler()
             self.tabBarController?.selectedViewController = self.tabBarController?.viewControllers?.first
             
         })
@@ -43,13 +56,6 @@ class DeleteBoardVC: UIViewController {
         deleteBoardAC.addAction(deleteAction)
         present(deleteBoardAC, animated: true, completion: nil)
     }
-    
-    func configureApp(){
-        //Check connectivity to show/hide shared button in navigation bar
-        currentBoard = gameImpl.getBoard()
-    }
-    
-    
     
     
 }
---
+++ ArretoApp/MainVCHandler.swift
@@ -0,0 +1,13 @@
+//
+//  MainVCHandler.swift
+//  ArretoApp
+//
+//  Created by Rudy E Matos on 1/3/17.
+//  Copyright © 2017 Bearded Gentleman. All rights reserved.
+//
+
+import Foundation
+
+protocol MainVCHandler{
+    func removeAllRecordsHandler()
+}
